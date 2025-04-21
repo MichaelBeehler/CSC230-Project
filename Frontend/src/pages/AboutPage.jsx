1a969ddbdb194da064cc07373c157ba6d1fdbfd3
MichaelBeehler <159819216+MichaelBeehler@users.noreply.github.com>
2025-04-20 23:17:01 -0400

    Update to rename the tab in browser

--- Frontend/src/pages/AboutPage.jsx
+++ Frontend/src/pages/AboutPage.jsx
@@ -6,6 +6,15 @@ function AboutPage() {
       <h2>Mission Statement</h2>
       <p className="paragraph">It is the mission of the Department of Criminology and Criminal Justice to provide criminological education, research and services to students, practitioners, policymakers and the community through an intellectually challenging environment that promotes collegiality and an educational experience relevant to crime and criminological theory that is meant to engage intellectual inquiry with domestic and international perspectives.</p>
       <h2>Department of Criminology and Criminal Justice Purposes/Goals</h2>
+
+      <p className="paragraph">The CIRT supports the overall vision of The University by further strengthening existing programs and departments, furthering the training and success of graduate students, increasing the recognition of the Department of Criminology and Criminal Justice as well as UT, while also creating external funding opportunities.
+
+The CIRT has four specific functions:
+
+Encourage networking and collaboration between UT scholars and criminal justice agencies and businesses;
+Provide learning resources and training for students, faculty and criminal justice professionals;
+Prepare students for future careers in the ever-changing field of criminal justice; and
+Provide a central hub for the intersection of research, training and student engagement within the Department of Criminology and Criminal Justice. </p>
     </div>
   );
 }
