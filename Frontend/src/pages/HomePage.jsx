924f59f20929c6adc386c33f5529ecb95bd3e1b3
MichaelBeehler <159819216+MichaelBeehler@users.noreply.github.com>
2025-05-04 20:02:50 -0400

    Update HomePage.jsx

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import RecentSubmissions from "../components/RecentSubmissions";
 import SearchBar from "../components/SearchBar";
 import "./HomePage.css";
-const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 const HomePage = () => {
   return (
