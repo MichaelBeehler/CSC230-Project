00d02296586c74ba0fd71c49da962989383f4ade
Your GitHub Username <your.email@example.com>
2025-05-06 03:49:05 -0400

    Footer

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -3,49 +3,21 @@ import { Link } from "react-router-dom";
 import "./Footer.css";
 
 function Footer() {
-  // Google Maps URL for University of Tampa
-  const mapsUrl = "https://maps.google.com/?q=University+of+Tampa,401+W.+Kennedy+Blvd,Tampa,FL+33606";
-    
   return (
     <footer className="footer">
-      <div className="footer-container">
-        {/* Left Side */}
+      <div className="footer-content">
         <div className="footer-left">
-          <h3>UNIVERSITY OF TAMPA</h3>
-                    
-          {/* Clickable address that opens in Maps */}
-          <a 
-            href={mapsUrl}
-            target="_blank" 
-            rel="noopener noreferrer"
-            className="address-link"
-          >
-            <div>401 W. Kennedy Blvd</div>
-            <div>Tampa, FL 33606-1490</div>
-          </a>
-                    
-          {/* Clickable phone number */}
-          <a 
-            href="tel:+18132533333" 
-            className="phone-link"
-          >
-            (813) 253-3333
-          </a>
+          <p>University of Tampa</p>
+          <p>401 W. Kennedy Blvd</p>
+          <p>Tampa, FL 33606-1490</p>
+          <p>(813) 253-3333</p>
         </div>
-                
-        {/* Right Side */}
         <div className="footer-right">
-          <h4>Quick Links</h4>
-          <Link to="/">Home</Link>
-          <Link to="/contact">Contact Us</Link>
-          <Link to="/about">About Us</Link>
+          <p>Quick Links</p>
+          <p><Link to="/">Home</Link></p>
+          <p><Link to="/contact">Contact Us</Link></p>
+          <p><Link to="/about">About Us</Link></p>
         </div>
-      </div>
-            
-      {/* Bottom Section */}
-      <div className="footer-bottom">
-        <p>© 2025 University of Tampa. All rights reserved.</p>
-        <p>Maintained by Department of Criminology</p>
       </div>
     </footer>
   );
