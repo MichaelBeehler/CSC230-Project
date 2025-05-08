f6d98d7f691b20ba5b2b88c83b66f03c46159f8d
aidbus <atarpinianco@gmail.com>
2025-05-08 00:38:54 -0400

    RecentSubmissions styling change

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -9,7 +9,7 @@ const HomePage = () => {
       <div className="background-image" />
         <div className="home-content">
           <div style={{ textAlign: "center", padding: "20px"}}>
-          <h1>Welcome to the CIRT</h1>
+          <h1>Welcome to the CIRT Academic Journal!</h1>
           <p>Search for approved publications below:</p>
 
           <SearchBar />
